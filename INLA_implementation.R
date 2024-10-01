b2c1c84b98da8214578be2053b7e0d0f09d15ace
Sara Martino <sara.martino@ntnu.no>
2024-10-01 10:29:07 +0200

    small changes

--- INLA_implementation.R
+++ INLA_implementation.R
@@ -3,11 +3,7 @@ library(lubridate)
 library(tidyverse)
 library(INLA)
 library(fmesher)
-
-
-
 library(sf)
-library(inlabru)
 
 rm(list = ls())
 
@@ -78,28 +74,27 @@ inla.Occupancy_detCov <- function(X_det){
     arrange(Year, Site) %>%
     mutate(x = EAST/1000,
            y = NORTH/1000)
-
+  
   # Add detection covariates
   data$relativeListLength <- data$listL / data$maxListlArea
   
   data$relativeListLength <- (data$relativeListLength-
                                 mean(data$relativeListLength)) / sd(data$relativeListLength)
-
-
- 
+  
+  
+  
 }
 
 
 
 ## Occupancy data ----------------------------------------------------------
 
-  
+
 data_filter = data %>% 
-    filter(month %in% c(5,6,7,8)) %>%
-    group_by(SamplUnit) %>% 
-    #filter(row_number() <= 90) %>% 
-    mutate(yy = seq_along(x)) %>% 
-    ungroup() 
+  filter(month %in% c(5,6,7,8)) %>%
+  group_by(SamplUnit) %>% 
+  mutate(yy = seq_along(x)) %>% 
+  ungroup() 
 
 data_filter  = data_filter %>%
   mutate(JulianDate1 = scale(jDate),
@@ -137,42 +132,133 @@ relativeListLength = data_filter%>%
 X_det <- list(
   as.matrix(julian1[,-1]) ,
   as.matrix(julian2[,-1]),
- # as.matrix(julian3[,-1]),
+  # as.matrix(julian3[,-1]),
   as.matrix(relativeListLength[,-1])
-  ) %>% 
+) %>% 
   inla.Occupancy_detCov()
 
 
 
+# temporal model without spatial effect  ------------------------------------------------------------
+
+if(0)
+{
+  # This is hte model in the RSS-A paper 
+  
+  X_det <- list(
+    as.matrix(julian1[,-1]) ,
+    as.matrix(julian2[,-1]),
+    #as.matrix(julian3[,-1]),
+    as.matrix(relativeListLength[,-1])
+  ) %>% 
+    inla.Occupancy_detCov()
+  
+  
+  
+  X_occ2 <- Occ[,c(1:4)] %>% 
+    mutate(Int_occ =1) 
+  
+  
+  data_list = as.list(X_occ2)
+  data_list$Y = Y
+  data_list$X = X_det
+  
+  formula_time <- inla.mdata(Y,X) ~  -1 + Int_occ +  f(Year, model = "iid", 
+                                                       fixed = F, initial  = 0, constr = T)
+  model_time <- inla(formula_time,    # model formula
+                     data= data_list,        # data 
+                     family= 'occupancy', # model likelihood
+                     #control.inla = list(int.strategy = "eb"),
+                     
+                     # priors
+                     control.fixed =  list(prec = 1, prec.intercept = 1),
+                     control.mode = list(theta = c(0.13521223 , 0.09538584, -5.31917496,
+                                                   1.75581385,  0)),
+                     # compute WAIC and DIC
+                     control.compute = list(dic = TRUE, waic = TRUE, config = TRUE),
+                     verbose = TRUE,
+                     # choose link functions for:
+                     # (i) the state process (control.link)
+                     # (ii) the observation process (link.simple)
+                     control.family = list(control.link = list(model = "logit"),
+                                           link.simple = "logit",
+                                           # priors for hyperparameters
+                                           hyper = list(
+                                             # Detection intercept prior 
+                                             beta1 = list(param = c(0,100)),
+                                             # Covariate 1 effect prior
+                                             beta2 = list(param = c(0,100)),
+                                             # Covariate 2 effect prior 
+                                             beta3 = list(param = c(0,100)),
+                                             beta4 = list(param = c(0,100)),
+                                             beta5 = list(param = c(0,100))
+                                           )
+                     ))
+  
+  samples_time = inla.posterior.sample(300, model_time)
+  
+  ## year spec intercept -----------------------------------------------------
+  
+  func_int = function(...)
+  {
+    out = inla.link.invlogit(Year)
+    out
+  }
+  
+  out = inla.posterior.sample.eval(func_int, samples_time) 
+  time_effect = data.frame(time = sort(unique(data$Year)),
+                           mean = apply(out,1,quantile, 0.5),
+                           q1 = apply(out,1,quantile, 0.025),
+                           q2 = apply(out,1,quantile, 0.975))
+  time_effect %>%
+    ggplot() + 
+    geom_point(aes(time,mean)) +
+    geom_errorbar(aes(time, ymin = q1, 
+                      ymax =  q2)) +
+    ylim(0,1) + ggtitle("Year specific intercept") +
+    ylab("Probability of Occurrence") + xlab("")
+  
+  ## Detection function ------------------------------------------------------
+  
+  
+  xx = yday(as.Date("01/05/2012", "%d/%m/%y")):yday(as.Date("31/08/2012", "%d/%m/%y"))
+  dd = cbind(1, 
+             scale(xx),
+             scale(xx)^2,
+             scale(xx)^3)    
+  
+  
+  data.frame(x = parse_date_time(x = xx, orders = "j") ,
+             m3 = inla.link.invlogit(dd[,1:3] %*% model_time$summary.hyperpar$mean[1:3]),
+             q12 = inla.link.invlogit(dd[,1:3] %*% model_time$summary.hyperpar$`0.025quant`[1:3]),
+             q22 = inla.link.invlogit(dd[,1:3] %*% model_time$summary.hyperpar$`0.025quant`[1:3])) %>%
+    ggplot() + 
+    geom_line(aes(x,m3)) +
+    geom_ribbon(aes(x, ymin = q12, ymax = q22), alpha = 0.5) +
+    ylab("Probability") + xlab("") + ggtitle("Probability of Detection")
+  
+}
 
 
 
-# Mesh -------------------------------------------------------
 
-# uk_sf = read_sf("shapefile/uk.shp")
-# uk_simplify = st_simplify(uk_sf, dTolerance = 3.6)
-# 
-# data_sf = st_as_sf(data_filter %>% distinct(Site, .keep_all = TRUE), coords = c("x","y"))
-# st_crs(data_sf) = st_crs(boundary)
+# create the Mesh -------------------------------------------------------
 
-
+data_sf = st_as_sf(data_filter %>% distinct(Site, .keep_all = TRUE), coords = c("x","y"))
 boundary = fm_nonconvex_hull(data_sf, convex = -0.03)
 
 mesh = fm_mesh_2d(boundary = boundary, max.edge = c(25,120),
-                  offset = c(10,80),
-                #  crs = st_crs(uk_simplify)
-                )
-ggplot() + geom_sf(data = uk_simplify) + 
-  gg(mesh)
-
-#ggplot() + geom_sf(data = boundary) + geom_sf(data = data_sf, pch =".") 
+                  offset = c(10,80))
+plot(mesh)
 
 
 
 
 # FULL MODEL  --------------------------------------------------------------
-# space time model 
+# This model adds a spatial effect for each year
 
+# NB: This takes quite some memory, so it is recommended that one runs it on a "new" R session
+# without running the time model
 matern <- inla.spde2.pcmatern(mesh,
                               prior.range = c(150, 0.5),
                               prior.sigma = c(1, 0.5))
@@ -181,7 +267,7 @@ matern <- inla.spde2.pcmatern(mesh,
 X_det <- list(
   as.matrix(julian1[,-1]) ,
   as.matrix(julian2[,-1]),
- # as.matrix(julian3[,-1]),
+  # as.matrix(julian3[,-1]),
   as.matrix(relativeListLength[,-1])
 ) %>% 
   inla.Occupancy_detCov()
@@ -211,9 +297,6 @@ data_list$X = X_det
 
 # INLA implementation -----------------------------------------------------
 
-
-if(0)
-{
 formula2 <- inla.mdata(Y,X) ~   -1 + #Int_occ +  
   f(spatial_field, model=matern,  A.local = A_sp2, replicate = time) +
   f(Year, x_scale, model= "iid", initial = 0, fixed = T,  constr = F) + 
@@ -230,7 +313,7 @@ model2 <- inla(formula2,    # model formula
                #control.inla=list(int.strategy='eb'),
                # compute WAIC and DIC
                control.compute = list(dic = TRUE, waic = TRUE, config = TRUE),
-               verbose = TRUE#,
+               verbose = TRUE,
                # choose link functions for:
                # (i) the state process (control.link)
                # (ii) the observation process (link.simple)
@@ -250,44 +333,5 @@ model2 <- inla(formula2,    # model formula
                                        # Covariate 2 effect prior
                                        beta6 = list(param = c(0,500))
                                      ))
-               )
-
-}
-
-# inlabru implementation --------------------------------------------------
-
-
-
-library(inlabru)
-
-data_list$yy = rnorm(dim(data_list$Y)[1])
-cmp = ~ -1 + #Int_occ +  
-  space(cbind(x,y), model=matern,   replicate = time) +
-  easting(Year, x_scale, model= "iid", initial = 0, fixed = T,  constr = F) + 
-  northing(Year, y_scale, model= "iid", initial = 0, fixed = T,  constr = F) + 
-  time(Year, model= "iid", initial = 0, fixed = T,  constr = F)
-
-lik = like(formula = inla.mdata(Y,X) ~ .,
-           family= 'occupancy',
-           data= data_list,
-           control.family = list(control.link = list(model = "logit"),
-                                 link.simple = "logit",
-                                 # priors for hyperparameters
-                                 hyper = list(
-                                   # Detection intercept prior
-                                   beta1 = list(param = c(0,500)),
-                                   # Covariate 1 effect prior
-                                   beta2 = list(param = c(0,500)),
-                                   # Covariate 2 effect prior
-                                   beta3 = list(param = c(0,500)),
-                                   beta4 = list(param = c(0,500)),
-                                   # Covariate 1 effect prior
-                                   beta5 = list(param = c(0,500)),
-                                   # Covariate 2 effect prior
-                                   beta6 = list(param = c(0,500))
-                                 ))
-           )
-
-bru_options_set(verbose = T, bru_verbose = 4)
-mod = bru(cmp, lik)
+)
 
